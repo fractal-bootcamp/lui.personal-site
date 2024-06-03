ba2073558a7c55c4fdacf46f13601ff9ae06603c
yab <hello@factions.ai>
2024-06-03 17:25:23 -0400

    attempt at slider code that I will imminently delete

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -3,7 +3,7 @@ import { Header, Socials } from "./_index";
 const ProjectPost = ( props : any ) => {
     const { projectName, projectDetail, imagePath, targetUrl } = props;
     return(
-        <>
+        <div>
             <div className = "relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
                 <div className = "relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white shadow-lg bg-clip-border rounded-xl h-80">
                     <a href={targetUrl}>
@@ -19,7 +19,7 @@ const ProjectPost = ( props : any ) => {
                     </p>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
@@ -28,11 +28,103 @@ const ProjectPost = ( props : any ) => {
 
 
 
+import React, { useRef } from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
+const ProjectCarousel = ({ sliderRef } : any) => {
+    const settings = {
+        dots: true, 
+        infinite: true,
+        speed: 500,
+        slidesToShow:2,
+        slidesToScroll: 2
+        // autoplay: true,
+        // autoplaySpeed: 6000
+    };
+    console.log("is this failing")
+    let allProjectDetails = [
+        {
+            projectName: "Spruce Plantation",
+            projectDetail: "400 trees in South Wicklow",
+            imagePath: "./images/spruce.png",
+            targetUrl: "en.wikipedia.org/wiki/Lough_Dan" ,
+        },
+        {
+            projectName: "Beautiful Decay",
+            projectDetail: "Snapshots of post-soviet nostalgia",
+            imagePath: "./images/decay.png",
+            targetUrl: "en.wikipedia.org/wiki/Nostalgia_for_the_Soviet_Union",
+        },
+    ]
+    console.log(allProjectDetails)
+
+    return(
+        <Slider ref={sliderRef} {...settings}>
+            {allProjectDetails.map((project) => (
+                <div>
+                    <ProjectPost projectName = {project.projectName} projectDetail = {project.projectDetail} imagePath = {project.imagePath} targetUrl = {project.targetUrl} />
+                </div>
+            )
+        )}
+        </Slider>
+    )
+}
+
+
+
+const SimpleCarousel = ({ sliderRef } : any) => {
+    const settings = {
+        dots: true, 
+        infinite: true,
+        speed: 500,
+        slidesToShow:2,
+        slidesToScroll: 2
+        // autoplay: true,
+        // autoplaySpeed: 6000
+    };
+
+    // const allProjectDetails = [
+    //     {
+    //         projectName: "Spruce Plantation",
+    //         projectDetail: "400 trees in South Wicklow",
+    //         imagePath: "./images/spruce.png",
+    //         targetUrl: "en.wikipedia.org/wiki/Lough_Dan" ,
+    //     },
+    //     {
+    //         projectName: "Beautiful Decay",
+    //         projectDetail: "Snapshots of post-soviet nostalgia",
+    //         imagePath: "./images/decay.png",
+    //         targetUrl: "en.wikipedia.org/wiki/Nostalgia_for_the_Soviet_Union",
+    //     },
+    // ]
+    // console.log(allProjectDetails)
+
+    return(
+        <Slider ref={sliderRef} {...settings}>
+            <div>
+                <img src="./images/elm.jpg" width="50" />
+            </div>
+            <div>
+                <img src="./images/elm.jpg" width="50" />
+            </div>
+            <div>
+                <img src="./images/elm.jpg" width="50" />
+                <p>idoafjewoi</p>
+            </div>
+            <div>
+                <img src="./images/elm.jpg" width="50" />
+                <p>idoafjewoi</p>
+            </div>
+        </Slider>
+    )
+}
 
 
 
 export default function Projects() {
+    const sliderRef = useRef(null);
     return (
         <div>
         <Header />
@@ -44,6 +136,9 @@ export default function Projects() {
         <br />
         <ProjectPost projectName = "Beautiful Decay" projectDetail = "Snapshots of post-soviet nostalgia" imagePath = "./images/decay.png" targetUrl = "en.wikipedia.org/wiki/Nostalgia_for_the_Soviet_Union" />
 
+        <SimpleCarousel sliderRef = {sliderRef} />
+
+        {/* <ProjectCarousel sliderRef = {sliderRef} /> */}
 
         <Socials />
         </div>
